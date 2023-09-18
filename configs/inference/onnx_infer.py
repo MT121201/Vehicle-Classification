95a034d37f9407af9aad2971c8bbe8889beabcab
MT121201 <triet.kieu_binu@hcmut.edu.vn>
2023-09-18 08:39:47 +0700

    update

---
+++ configs/inference/onnx_infer.py
@@ -0,0 +1,8 @@
+model = "/checkpoints/vehicle_cls/pretrain_repvgg.onnx"
+class_names = ['xe sedan', 'xe SUV', 'xe ban tai', 'xe ba gac', 'xe tai nho',
+                'xe tai lon', 'xe container', 'xe may xuc, may cau, xe lu, xe cho be tong',
+                'xe 16 cho', 'xe 29-32 cho', 'xe 52 cho']
+
+mean = [0, 0, 0]
+std=[255, 255, 255]
+size = (224, 224)
