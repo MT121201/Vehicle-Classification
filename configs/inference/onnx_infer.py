922446f4958c0705682353c7e05480ea14275cc4
MT121201 <triet.kieu_binu@hcmut.edu.vn>
2023-09-18 08:42:08 +0700

    update

--- configs/inference/onnx_infer.py
+++ configs/inference/onnx_infer.py
@@ -1,8 +1,10 @@
+# path to onnx model
 model = "/checkpoints/vehicle_cls/pretrain_repvgg.onnx"
 class_names = ['xe sedan', 'xe SUV', 'xe ban tai', 'xe ba gac', 'xe tai nho',
                 'xe tai lon', 'xe container', 'xe may xuc, may cau, xe lu, xe cho be tong',
                 'xe 16 cho', 'xe 29-32 cho', 'xe 52 cho']
-
+# mean and std
 mean = [0, 0, 0]
 std=[255, 255, 255]
+# input size of model
 size = (224, 224)
